b0eeddc7f83cda167185daec197712cf69889281
Natasha Das <nd367@njit.edu>
2022-11-24 18:52:54 -0500

    fixing error2

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-import os.math
+import os.path
 
 from qrcodegenerator import create_qr_code_image
 from config import Config
@@ -11,7 +10,7 @@ def main():
     directory_path_and_file_name = os.path.join(full_path, Config.QR_CODE_IMAGE_DIRECTORY,
                                                 Config.QR_CODE_DEFAULT_FILE_NAME)
     qr_image = create_qr_code_image(Config.QR_CODE_DEFAULT_URL)
-    for i in range(0,1):
+    for i in range(0, 1):
         while True:
             try:
                 qr_image.save(directory_path_and_file_name)
